dbf75e4b66b0a196522ae5afca3412242c092fad
kvnptl <kevinpatel4400@gmail.com>
2021-09-18 22:02:42 +0200

    new: edx_course module practice files

---
+++ cpp/edx_intro_course/module_2/complex_data_types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int a;
+
+    std::cout << "Hello world!" << std::endl;
+
+    // The compiler will initialize the remaining elements to the default value for the data type the array holds. 
+    // In this case, int data type, the remaining values are initialized to 0.
+    int int_array[10] = {1, 2, 3};
+    double double_ary[10] = {1.0, 2.1, 3};
+
+
+}
